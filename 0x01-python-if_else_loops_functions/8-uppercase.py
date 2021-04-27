d3a8090e5c851b97ccd481b1272faba28558800a
coosoti <charlesosoti@gmail.com>
2021-04-27 11:43:33 +0300

    added a script that prints a string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        a = ord(str[i])
+        if a >= 97 and a <= 123:
+            a = a - 32
+        print("{}".format(chr(a)), end='')
+    print()
