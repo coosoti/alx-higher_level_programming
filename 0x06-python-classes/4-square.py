95360d36881da6db93613c5162656cb949e26880
coosoti <charlesosoti@gmail.com>
2021-05-18 13:42:08 +0300

    added a class Square that defines a square by: (based on 3-square.py)
    Private instance attribute: size:
    property def size(self): to retrieve it
    property setter def size(self, value): to set it:
    size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
    if size is less than 0, raise a ValueError exception with the message size must be >= 0
    Instantiation with optional size: def __init__(self, size=0):
    Public instance method: def area(self): that returns the current square area

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+""" Documentation for a square class"""
+
+
+class Square:
+    """Represent a square class of quadrilateral with four equal sides
+
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """Initializes a new square
+
+        Args:
+            size (int): size of a side of a square
+
+        Returns:
+            None
+
+        Raises:
+            ValueError: When the value passed is less than 0
+            TypeError: When the value passed is not an integer
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculates the area of a square
+
+        Returns:
+            The area of the square
+        """
+        return (self.__size) ** 2
+
+    @property
+    def size(self):
+        """returns the current size of the object but if called
+        with a value the setter function overwrites the current size
+        with the size passed
+
+        Returns:
+            The size of the current object
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """resets the size of the object with the value passed
+
+        Args:
+            value (int): the new size of the square object
+
+        Raises:
+            ValueError: When the value passed is less than 0
+            TypeError: When the value passed is not an integer
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
