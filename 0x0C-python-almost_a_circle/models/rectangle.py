47c190344adc1922b301688c19eafc88e08a2d36
coosoti <charlesosoti@gmail.com>
2021-06-07 16:01:58 +0300

    added attributes for all setter methods and instantiation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,3 +23,79 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """getter of width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for the width attribute
+
+        Args:
+            value (int): value to set to width to
+        """
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter of height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for the height attribute
+
+        Args:
+            value (int): the value to set the height to
+        """
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """getter of x attribute"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter for the x coordinate attribute
+
+        Args:
+            value (int): the value to set the x attribute to
+        """
+
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """getter of y attribute"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter of the y coordinate attribute
+
+        Args:
+            value (int): the value to set the y attribute to
+        """
+
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
