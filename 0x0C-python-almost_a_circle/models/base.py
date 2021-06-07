0eef5f49fa20adb060ee079a1d6aa98dd0251a47
coosoti <charlesosoti@gmail.com>
2021-06-08 00:31:29 +0300

    added static method def from_json_string(json_string): that returns the list of the JSON string representation json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,6 +31,13 @@ class Base:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs to a file
