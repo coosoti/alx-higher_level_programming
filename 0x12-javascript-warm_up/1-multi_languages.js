bc6b6dc74d06649e2e775cc593d146f0cf5c92ed
coosoti <charlesosoti@gmail.com>
2021-08-02 09:20:10 +0300

    added a script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Prints 3 lines
+
+const firstLine = 'C is Fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+
+console.log(firstLine + '\n' + secondLine + '\n' + thirdLine);
