df0c58409016c2922c8544846f3c1a2c17c8b638
coosoti <charlesosoti@gmail.com>
2021-08-19 22:43:30 +0300

    added a Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response
-curl $1 -w '%{http_code}' -o /dev/null
+curl "$1" -w '%{http_code}' -so /dev/null
