503c793d56a894e59d6189977f1fcc7774291c1d
coosoti <charlesosoti@gmail.com>
2021-08-19 15:51:11 +0300

    added  a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+# This Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl $1 -s -X "X-HolbertonSchool-User-Id: 98"
